66df4898dbb699766e3ae72546ba0a8729d0cd7b
Priya11gupta <74971761+Priya11gupta@users.noreply.github.com>
2022-10-24 11:58:58 +0530

    Create duplicate.cpp

---
+++ DSA cpp/duplicate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int findDuplicate(vector < int > & nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
+int main() {
+  vector < int > arr;
+  arr = {1,3,4,2,3};
+  cout << "The duplicate element is " << findDuplicate(arr) << endl;
+}
